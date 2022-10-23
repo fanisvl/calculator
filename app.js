2b700f9e3812af34eacbee8e6eabf434e139b20f
fanisvl <fanis.vlahogiannis@gmail.com>
2022-10-23 17:39:01 +0300

    select buttons in js

--- app.js
+++ app.js
@@ -28,3 +28,21 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+let display = document.querySelector('.display');
+let num0 = document.querySelector('#0')
+let num1 = document.querySelector('#1')
+let num2 = document.querySelector('#2')
+let num3 = document.querySelector('#3')
+let num4 = document.querySelector('#4')
+let num5 = document.querySelector('#5')
+let num6 = document.querySelector('#6')
+let num7 = document.querySelector('#7')
+let num8 = document.querySelector('#8')
+let num9 = document.querySelector('#9')
+let add = document.querySelector('#+')
+let sub = document.querySelector('#-')
+let mul = document.querySelector('#*')
+let div = document.querySelector('#/')
+let displayValue;
+
